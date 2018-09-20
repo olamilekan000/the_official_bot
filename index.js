c9d6305870b695048199ab38c9443fcce73e736f
Olamilekan Odukoya <olarpringo@gmail.com>
2018-09-20 03:43:47 +0100

    firebase setup

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
 
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+admin.initializeApp();
 const app = express();
 
 app.use(bodyParser.json());
@@ -10,4 +13,5 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(require('./route/index'));
 
+exports.api = functions.https.onRequest(app);
 app.listen(process.env.PORT || 9001, () => { console.log("server's up!") });
